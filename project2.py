466a509ade81c252f43e66e8e4d32aa7624fc905
Motunrayo Lawrence <156985370+LawMot@users.noreply.github.com>
2026-03-16 15:27:09 -0700

    Add files via upload

--- project2.py
+++ project2.py
@@ -10,7 +10,7 @@ def forward_selection(data):
     for i in range(1, num_features + 1):
         print(f"On level {i} of the search tree")
         added_feature = None
-        best = 0
+        best = -1
 
         #inner loop to test each feature
         for j in range(1, num_features + 1):
@@ -26,14 +26,36 @@ def forward_selection(data):
                     added_feature = j
         if added_feature != None:
             curr.append(added_feature)
-            print(f"On level {i} i added feature {added_feature} to current set(Accuracy: {best:.2%})\n")
+            print(f"On level {i} added feature {added_feature} to current set(Accuracy: {best:.2%})\n")
 
     return curr
+
+def backward_elimination(data):
+    # col 0 is the label
+    num_features = data.shape[1] - 1
+    curr = list(range(1,num_features+1)) #start with all variables
+    best_subset = []
+
+    for i in range(curr):
+        remove_feature = None
+        best = -1
+
+        for j in range(curr):
+            test_feature = curr - [j]
+            accuracy = leave_one_out(data, test_feature)
+            if accuracy > best:
+                best = accuracy
+                remove_feature = j
+        if remove_feature != None:
+            best_subset.append(i)
+    return best_subset
+
+
 
 #leave one out cross-validation
 # using nearest neighbor classifier 
 def leave_one_out(data, subset):
-    # correct used to keep track of objects corectly identified
+    # correct used to keep track of objects correctly identified
     correct = 0
     size = len(data)
 
@@ -53,7 +75,7 @@ def leave_one_out(data, subset):
         for j in range(size):
             if j != i:
                 # euclidean distance
-                dist = np.sqrt(np.sum((classify_object -features[j])**2))
+                dist = np.sqrt(np.sum((classify_object - features[j])**2))
 
                 if dist < nearest_dist:
                     nearest_dist = dist
@@ -62,10 +84,8 @@ def leave_one_out(data, subset):
             correct +=1
     return correct/size # return accuracy percentage
 
-
-
-def main(file):
-    print("Welcome to Motunrayo Lawrence Feature Selection Algorithm!")
+def main():
+    print("Welcome to Motunrayo Lawrence's Feature Selection Algorithm!")
 
     file = input (
         "Type in the name of the file to test:\n"
@@ -83,7 +103,7 @@ def main(file):
     if (algorithm == "1"):
         forward_selection(data)
 
-    # elif(algorithm == 2):
-    #     backward_elimination(file)
+    elif(algorithm == 2):
+        backward_elimination(data)
 
 main()
